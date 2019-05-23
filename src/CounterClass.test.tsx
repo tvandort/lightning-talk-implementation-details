ef45f11b89174a6a3db677ab449dc33951ca7740
Tom <tvandort1@gmail.com>
2019-05-23 12:22:19 -0400

    Better describes.

--- src/CounterClass.test.tsx
+++ src/CounterClass.test.tsx
@@ -3,19 +3,19 @@ import Counter from "./CounterClass";
 import { shallow } from "enzyme";
 
 describe(Counter, () => {
-  describe("test by calling directly", () => {
+  describe("test using state", () => {
     it("can add", () => {
       const wrapper = shallow<Counter>(<Counter />);
     });
   });
 
-  describe("test by clicking", () => {
+  describe("test by clicking check state", () => {
     it("can add", () => {
       const wrapper = shallow<Counter>(<Counter />);
     });
   });
 
-  describe("test what happens", () => {
+  describe("test drive", () => {
     it("can add", () => {
       const wrapper = shallow<Counter>(<Counter />);
     });
