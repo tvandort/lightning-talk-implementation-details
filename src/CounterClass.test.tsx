fcb78dd2ddb38ca5b14914f8d493d8f1520bcafa
Tom <tvandort1@gmail.com>
2019-05-23 12:21:32 -0400

    Better starting point.

--- src/CounterClass.test.tsx
+++ src/CounterClass.test.tsx
@@ -2,4 +2,22 @@ import React from "react";
 import Counter from "./CounterClass";
 import { shallow } from "enzyme";
 
-describe(Counter, () => {});
+describe(Counter, () => {
+  describe("test by calling directly", () => {
+    it("can add", () => {
+      const wrapper = shallow<Counter>(<Counter />);
+    });
+  });
+
+  describe("test by clicking", () => {
+    it("can add", () => {
+      const wrapper = shallow<Counter>(<Counter />);
+    });
+  });
+
+  describe("test what happens", () => {
+    it("can add", () => {
+      const wrapper = shallow<Counter>(<Counter />);
+    });
+  });
+});
